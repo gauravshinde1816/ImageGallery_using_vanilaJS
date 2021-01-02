506e393e39d66ab153c216c6fe8853075a242278
gauravshinde1816 <gauravshinde1816@gmail.com>
2021-01-02 08:49:50 +0530

    Image Gallery using VanilaJS

---
+++ main.js
@@ -0,0 +1,21 @@
+const [current, imgs] = [
+  document.querySelector("#current"),
+  document.querySelectorAll(".imgs img"),
+];
+imgs[0].style.opacity = 0.4;
+
+const opacity = 0.4;
+const imageClick = (e) => {
+  imgs.forEach((img) => (img.style.opacity = 1));
+
+  current.src = e.target.src;
+
+  current.classList.add("fade-in");
+  setTimeout(() => {
+    current.classList.remove("fade-in");
+  }, 500);
+
+  e.target.style.opacity = opacity;
+};
+
+imgs.forEach((img) => img.addEventListener("click", imageClick));
